c5cd48407343945795e2584ed04e9dfa7a7cebc5
Fjoedar <fjoedar@gmail.com>
2023-11-21 19:10:12 +0100

    Barna

--- november21/Program.cs
+++ november21/Program.cs
@@ -11,6 +11,7 @@ namespace november21
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub");
+            Console.WriteLine("Hello Barna");
             Console.ReadLine();
         }
     }

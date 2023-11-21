0dce16c57f664ccead33d4971d486c76c2befab7
Fjoedar <fjoedar@gmail.com>
2023-11-21 19:13:19 +0100

    Változtatás

--- november21/Program.cs
+++ november21/Program.cs
@@ -11,7 +11,7 @@ namespace november21
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub");
-            Console.WriteLine("Hello Barna");
+            Console.WriteLine("Hello BSandor 46");
             Console.ReadLine();
         }
     }
